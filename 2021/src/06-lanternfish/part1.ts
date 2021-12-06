72dc508e5b9754bd8148a40e625065557b962686
Kellan Elliott-McCrea <kelliott@dropbox.com>
2021-12-06 09:07:10 -0500

    clean up

--- 2021/src/06-lanternfish/part1.ts
+++ 2021/src/06-lanternfish/part1.ts
@@ -3,10 +3,12 @@ import * as fs from 'fs';
 let input = fs.readFileSync(process.stdin.fd, 'utf-8');
 let lines = input.split('\n')
 
-let fish: string[] = lines[0].split(',') || []
+let fish:  Map<number, number>  = lines[0]
+	.split(',')
+	.map(n => +n)
+	.reduce( (acc, curr) => {
+		return acc.set(curr, (acc.get(curr) ?? 0) +1)
+	}, new Map<number, number>()
+);
 
-let fishDays: Map<string, number> = fish.reduce( (acc, curr) => {
-	return acc.set(curr, (acc.get(curr) ?? 0) +1)
-}, new Map<string, number>());
-
-console.log(fishDays);
+console.log(fish);
