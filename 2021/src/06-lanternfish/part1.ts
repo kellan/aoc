692154e0d63e425677a848ea726b4efe825d1fbb
Kellan Elliott-McCrea <kelliott@dropbox.com>
2021-12-06 09:53:35 -0500

    same code, part 1 and part 2

--- 2021/src/06-lanternfish/part1.ts
+++ 2021/src/06-lanternfish/part1.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 let input = fs.readFileSync(process.stdin.fd, 'utf-8');
-let lines = input.split('\n')
+let lines = input.split('\n');
 
 let fish:  Map<number, number>  = lines[0]
 	.split(',')
@@ -11,4 +11,31 @@ let fish:  Map<number, number>  = lines[0]
 	}, new Map<number, number>()
 );
 
-console.log(fish);
+
+for (let d = 0; d < 256; d++) {
+	// one day
+	let newFish: Map<number, number> = new Map();
+	for (let i = 8; i > 0; i--) {
+		if (fish.get(i)) {
+			newFish.set(i-1, fish.get(i) ?? 0);
+		}
+	}
+	if(fish.get(0)) {
+		newFish.set(8, fish.get(0) ?? 0);
+
+		newFish.set(6, 
+			(newFish.get(6) ?? 0) + (fish.get(0) ?? 0)
+		);
+	}
+	fish = newFish;
+}
+
+// why doesn't this work?
+//console.log(fish.values.reduce((acc, val) => acc+val));
+
+let sum = 0;
+fish.forEach( (val, key) => {
+	sum += val;
+});
+
+console.log(sum);
