b294352893aad89bab98e3a3e4f079dabcfb4712
kellan <kellan@pobox.com>
2022-12-01 08:31:56 -0500

    day 1, part 2

--- 2022/rust/day1-calorie-counting/src/main.rs
+++ 2022/rust/day1-calorie-counting/src/main.rs
@@ -1,24 +1,44 @@
 use std::io::{self, Read};
 
-fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer)?;
+fn main() {
+    //part1();
+    part2();
+}
 
-    let mut max = 0;
-    let mut elf = 0;
+fn lines() -> String {
+    let mut buffer = String::new();
+    _ = io::stdin().read_to_string(&mut buffer);
+    return buffer;
+}
+
+fn sums(buffer: String) -> Vec<i32> {
+    let mut elves = Vec::new();
+    let mut accum = 0;
 
     for line in buffer.lines() {
         if !line.trim().is_empty() {
             let num = line.parse::<i32>().unwrap();
-            elf += num;
+            accum += num;
         } else {
-            if elf > max {
-                max = elf;
-            }
-            elf = 0;
+            elves.push(accum);
+            accum = 0;
         }
     }
-    println!("{}", max);
 
-    Ok(())
+    elves
+}
+
+fn part2() {
+    let buffer = lines();
+    let mut sums = sums(buffer);
+    sums.sort();
+    sums.reverse();
+    println!("{}", sums[0] + sums[1] + sums[2]);
+}
+fn part1() {
+    let buffer = lines();
+    let mut sums = sums(buffer);
+    sums.sort();
+    sums.reverse();
+    println!("{}", sums[0]);
 }
