952ee8189918207301f84a710a3b6e362295b2c8
kellan <kellan@pobox.com>
2022-12-04 13:57:28 -0500

    refactor, inspired by @quiiver and @jvns

--- 2022/rust/day4-camp-cleanup/src/main.rs
+++ 2022/rust/day4-camp-cleanup/src/main.rs
@@ -1,72 +1,111 @@
-use itertools::Itertools;
 use std::io::{self, Read};
 
 fn main() {
-    //part1();
-    part2();
+    let input = read_stdin();
+    let ranges = parse_ranges(&input);
+    let answer1 = solve(&ranges, |l, r| {
+        l.start <= r.start && l.end >= r.end || l.start >= r.start && l.end <= r.end
+    });
+
+    let answer2 = solve(&ranges, |l, r| {
+        l.end >= r.start && l.start <= r.end || r.end >= l.start && r.start <= l.end
+    });
+
+    dbg!(answer1);
+    dbg!(answer2);
 }
 
-fn part1() {
-    let input = read_stdin();
-    let overlaps = input
-        .lines()
-        .map(|line| line.split(','))
-        .filter_map(|r| r.collect_tuple())
-        .map(|(left, right)| contained(left, right))
+fn solve(ranges: &Vec<(Range, Range)>, check: fn(&Range, &Range) -> bool) -> u32 {
+    let bools = ranges
+        .iter()
+        .map(|(left, right)| check(left, right))
         .filter(|x| *x)
         .collect::<Vec<bool>>();
 
-    //dbg!(&overlaps);
-    let answer = overlaps.len();
-    dbg!(answer);
+    bools.len() as u32
 }
 
-fn part2() {
-    let input = read_stdin();
-    let overlaps = input
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn from(input: &str) -> Range {
+        // 51-88
+        let mut parts = input.split('-');
+        let start = parts.next().unwrap().parse().unwrap();
+        let end = parts.next().unwrap().parse().unwrap();
+        Range { start, end }
+    }
+}
+
+fn parse_ranges(input: &str) -> Vec<(Range, Range)> {
+    input
         .lines()
-        .map(|line| line.split(','))
-        .filter_map(|r| r.collect_tuple())
-        .map(|(left, right)| overlaps(left, right))
-        .filter(|x| *x)
-        .collect::<Vec<bool>>();
-
-    //dbg!(&overlaps);
-    let answer = overlaps.len();
-    dbg!(answer);
+        .map(|line| {
+            let mut parts = line.split(',');
+            let left = Range::from(parts.next().unwrap());
+            let right = Range::from(parts.next().unwrap());
+            (left, right)
+        })
+        .collect()
 }
 
-fn contained(left: &str, right: &str) -> bool {
-    let left = left
-        .split('-')
-        .map(|i| i.parse().unwrap())
-        .take(2)
-        .collect::<Vec<u32>>();
+// fn part1() {
+//     let input = read_stdin();
+//     let ranges = parse_ranges(&input);
 
-    let right = right
-        .split('-')
-        .map(|i| i.parse().unwrap())
-        .take(2)
-        .collect::<Vec<u32>>();
+//     let answer = overlaps.len();
+//     dbg!(answer);
+// }
 
-    (left[0] <= right[0] && left[1] >= right[1]) || (right[0] <= left[0] && right[1] >= left[1])
-}
+// fn part2() {
+//     let input = read_stdin();
+//     let overlaps = input
+//         .lines()
+//         .map(|line| line.split(','))
+//         .filter_map(|r| r.collect_tuple())
+//         .map(|(left, right)| overlaps(left, right))
+//         .filter(|x| *x)
+//         .collect::<Vec<bool>>();
 
-fn overlaps(left: &str, right: &str) -> bool {
-    let left = left
-        .split('-')
-        .map(|i| i.parse().unwrap())
-        .take(2)
-        .collect::<Vec<u32>>();
+//     //dbg!(&overlaps);
+//     let answer = overlaps.len();
+//     dbg!(answer);
+// }
 
-    let right = right
-        .split('-')
-        .map(|i| i.parse().unwrap())
-        .take(2)
-        .collect::<Vec<u32>>();
+// fn contained(left: &str, right: &str) -> bool {
+//     let left = left
+//         .split('-')
+//         .map(|i| i.parse().unwrap())
+//         .take(2)
+//         .collect::<Vec<u32>>();
 
-    !(left[1] < right[0] || right[1] < left[0])
-}
+//     let right = right
+//         .split('-')
+//         .map(|i| i.parse().unwrap())
+//         .take(2)
+//         .collect::<Vec<u32>>();
+
+//     (left[0] <= right[0] && left[1] >= right[1]) || (right[0] <= left[0] && right[1] >= left[1])
+// }
+
+// fn overlaps(left: &str, right: &str) -> bool {
+//     let left = left
+//         .split('-')
+//         .map(|i| i.parse().unwrap())
+//         .take(2)
+//         .collect::<Vec<u32>>();
+
+//     let right = right
+//         .split('-')
+//         .map(|i| i.parse().unwrap())
+//         .take(2)
+//         .collect::<Vec<u32>>();
+
+//     !(left[1] < right[0] || right[1] < left[0])
+// }
 
 fn read_stdin() -> String {
     let mut buffer = String::new();
