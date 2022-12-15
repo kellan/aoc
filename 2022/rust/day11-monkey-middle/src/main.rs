cfc00ba1bd6b0a4cee8cf81ab9e2b90fcaffeebb
kellan <kellan@pobox.com>
2022-12-14 19:14:37 -0500

    part 2, day 11

--- 2022/rust/day11-monkey-middle/src/main.rs
+++ 2022/rust/day11-monkey-middle/src/main.rs
@@ -7,41 +7,56 @@ fn main() {
     let mut troop = parse(input);
 
     //troop.get_mut(&0).unwrap().tick_part1();
-    part1(troop, 20);
-    //part2(troop, 1);
+    //part1(troop, 20);
+    part2(troop, 10_000);
 }
 
-fn part1(mut troop: HashMap<u32, Monkey>, rounds: u32) {
+fn part1(mut troop: HashMap<u64, Monkey>, rounds: u64) {
     for _i in 0..rounds {
         for n in 0..troop.len() {
-            let distribute = troop.get_mut(&(n as u32)).unwrap().tick_part1();
+            //let distribute = troop.get_mut(&(n as u64)).unwrap().tick_part1();
+            let distribute = troop.get_mut(&(n as u64)).unwrap().tick(Some(3), None);
             //dbg!(distribute);
             for dist in distribute {
-                troop.get_mut(&(dist.0 as u32)).unwrap().items.push(dist.1)
+                troop.get_mut(&(dist.0 as u64)).unwrap().items.push(dist.1)
             }
         }
     }
 
-    let mut scores: Vec<u32> = troop.iter().map(|(k, v)| v.inspected).collect();
+    let mut scores: Vec<u64> = troop.iter().map(|(k, v)| v.inspected).collect();
     scores.sort();
     scores.reverse();
     println!("{}", scores[0] * scores[1]);
 }
 
-// fn part2(mut troop: HashMap<u32, Monkey>, rounds: u32) {
-//     let modulo: u32 = troop.iter().map(|k, v| v.divisor).product();
-//     println!("modulo {}", modulo);
-// }
+fn part2(mut troop: HashMap<u64, Monkey>, rounds: u64) {
+    let modulo: u64 = troop.iter().map(|(k, v)| v.divisor).product();
+
+    for _i in 0..rounds {
+        for n in 0..troop.len() {
+            let distribute = troop.get_mut(&(n as u64)).unwrap().tick(None, Some(modulo));
+            //dbg!(distribute);
+            for dist in distribute {
+                troop.get_mut(&(dist.0 as u64)).unwrap().items.push(dist.1)
+            }
+        }
+    }
+
+    let mut scores: Vec<u64> = troop.iter().map(|(k, v)| v.inspected).collect();
+    scores.sort();
+    scores.reverse();
+    println!("{}", scores[0] * scores[1]);
+}
 
 #[derive(Debug)]
 struct Monkey {
-    id: u32,
+    id: u64,
     operation: Operation,
-    items: Vec<u32>,
-    divisor: u32,
-    on_true: u32,
-    on_false: u32,
-    inspected: u32,
+    items: Vec<u64>,
+    divisor: u64,
+    on_true: u64,
+    on_false: u64,
+    inspected: u64,
 }
 
 impl Monkey {
@@ -57,12 +72,19 @@ impl Monkey {
         }
     }
 
-    fn tick_part1(&mut self) -> Vec<(u32, u32)> {
-        let mut distribute: Vec<(u32, u32)> = Vec::new();
+    fn tick(&mut self, bored: Option<u64>, modulo: Option<u64>) -> Vec<(u64, u64)> {
+        let mut distribute: Vec<(u64, u64)> = Vec::new();
         while let Some(item) = self.items.pop() {
             let mut worried = self.operation.inspect(item);
             self.inspected += 1;
-            worried = worried / 3;
+
+            if let Some(modulo) = modulo {
+                worried = worried % modulo;
+            }
+
+            if let Some(bored) = bored {
+                worried = worried / 3;
+            }
 
             if worried % self.divisor == 0 {
                 distribute.push((self.on_true, worried));
@@ -77,8 +99,8 @@ impl Monkey {
 
 #[derive(Debug)]
 enum Operation {
-    Multiply(u32),
-    Add(u32),
+    Multiply(u64),
+    Add(u64),
     Square,
     Noop,
 }
@@ -93,7 +115,7 @@ impl Operation {
         }
     }
 
-    fn inspect(&self, old: u32) -> u32 {
+    fn inspect(&self, old: u64) -> u64 {
         match self {
             Operation::Square => old * old,
             Operation::Add(num) => old + num,
@@ -103,9 +125,9 @@ impl Operation {
     }
 }
 
-fn parse(input: String) -> HashMap<u32, Monkey> {
+fn parse(input: String) -> HashMap<u64, Monkey> {
     let parts = input.split("\n\n");
-    let mut troop: HashMap<u32, Monkey> = HashMap::new();
+    let mut troop: HashMap<u64, Monkey> = HashMap::new();
 
     for part in parts {
         let mut monkey = Monkey::new();
@@ -115,7 +137,7 @@ fn parse(input: String) -> HashMap<u32, Monkey> {
             match (p[0], p[1]) {
                 ("Monkey", id) => monkey.id = to_num(id),
                 ("Starting", "items:") => {
-                    let items: Vec<u32> = p[2..].iter().map(|s| to_num(s)).collect();
+                    let items: Vec<u64> = p[2..].iter().map(|s| to_num(s)).collect();
                     monkey.items = items;
                 }
                 ("Operation:", _) => monkey.operation = Operation::new((p[3], p[4], p[5])),
@@ -132,7 +154,7 @@ fn parse(input: String) -> HashMap<u32, Monkey> {
     troop
 }
 
-fn to_num(input: &str) -> u32 {
+fn to_num(input: &str) -> u64 {
     String::from(input)
         .chars()
         .filter(|c| c.is_digit(10))
