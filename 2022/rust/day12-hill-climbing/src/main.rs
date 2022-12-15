e9c672330dc2d1fc96ae0ebf71ef2990a2f01c7e
kellan <kellan@pobox.com>
2022-12-15 10:48:36 -0500

    refactor neighbors to support walking downhill refactor bfs to take a list of targets

--- 2022/rust/day12-hill-climbing/src/main.rs
+++ 2022/rust/day12-hill-climbing/src/main.rs
@@ -3,23 +3,33 @@ use std::io::{self, Read};
 
 fn main() {
     let input = read_stdin();
-    let (grid, start, end) = parse(input);
+    let (grid, start, end, low_points) = parse(input);
     dbg!(&start, &end);
-    let (found, dist) = bfs(grid, start, end);
+    // part 1
+    // let end = HashSet::from([end]);
+    // let (found, dist) = bfs(grid, start, end, false);
+    // part 2
+    dbg!(&low_points);
+    let (found, dist) = bfs(grid, end, low_points, true);
     dbg!(found);
     dbg!(dist);
 }
 
-fn bfs(grid: HashMap<Point, u32>, start: Point, end: Point) -> (Point, u32) {
+fn bfs(
+    grid: HashMap<Point, u32>,
+    start: Point,
+    end: HashSet<Point>,
+    downhill: bool,
+) -> (Point, u32) {
     let mut q: VecDeque<(Point, u32)> = VecDeque::from([(start, 0)]);
     let mut visited: HashSet<Point> = HashSet::new();
 
     while let Some((p, dist)) = q.pop_front() {
-        if p == end {
+        if end.contains(&p) {
             return (p, dist);
         }
 
-        for neighbor in neighbors(&grid, &p) {
+        for neighbor in neighbors(&grid, &p, downhill) {
             if visited.contains(&neighbor) {
                 continue;
             }
@@ -31,7 +41,7 @@ fn bfs(grid: HashMap<Point, u32>, start: Point, end: Point) -> (Point, u32) {
     (Point(0, 0), 0)
 }
 
-fn neighbors(grid: &HashMap<Point, u32>, point: &Point) -> Vec<Point> {
+fn neighbors(grid: &HashMap<Point, u32>, point: &Point, downhill: bool) -> Vec<Point> {
     let mut neighbors: Vec<Point> = Vec::new();
     let height = *grid.get(point).unwrap() as i32;
 
@@ -47,7 +57,11 @@ fn neighbors(grid: &HashMap<Point, u32>, point: &Point) -> Vec<Point> {
         let dw = point.1 as i32 + delta.1;
         let n = Point(dh as u32, dw as u32);
         if let Some(n_height) = grid.get(&n) {
-            if *n_height as i32 - height <= 1 {
+            if downhill {
+                if height - *n_height as i32 <= 1 {
+                    neighbors.push(n);
+                }
+            } else if *n_height as i32 - height <= 1 {
                 neighbors.push(n);
             }
         }
@@ -58,10 +72,11 @@ fn neighbors(grid: &HashMap<Point, u32>, point: &Point) -> Vec<Point> {
 #[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct Point(u32, u32);
 
-fn parse(input: String) -> (HashMap<Point, u32>, Point, Point) {
+fn parse(input: String) -> (HashMap<Point, u32>, Point, Point, HashSet<Point>) {
     let mut grid: HashMap<Point, u32> = HashMap::new();
     let mut start = Point(0, 0);
     let mut end = Point(0, 0);
+    let mut low_points: HashSet<Point> = HashSet::new();
 
     for (i, line) in input.lines().enumerate() {
         for (j, c) in line.chars().enumerate() {
@@ -76,13 +91,16 @@ fn parse(input: String) -> (HashMap<Point, u32>, Point, Point) {
                 }
                 c => {
                     let p = Point(i as u32, j as u32);
-                    grid.insert(p, char_to_num(c));
+                    grid.insert(p.to_owned(), char_to_num(c));
+                    if c == 'a' {
+                        low_points.insert(p.to_owned());
+                    }
                 }
             }
         }
     }
     //dbg!(grid.get(&Point(2, 5)));
-    (grid, start, end)
+    (grid, start, end, low_points)
 }
 
 fn char_to_num(c: char) -> u32 {
