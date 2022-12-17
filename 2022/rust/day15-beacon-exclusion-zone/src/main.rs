9448997b2a32033ea8f052004733775b68c0f187
kellan <kellan@pobox.com>
2022-12-17 17:01:22 -0500

    for part 2 I _feel_ like I'm on the right track. i probably should have tried brute forcing all 4mx4m grid. instead i'm trying to build ranges, and merge them to reduce the number of checks (I suppose alternately I could have tried some sort of divide and conqueror approach finding blocks that fit inside a sensor's sweep)
    anyway it isn't working, and i need to stop and making dinner

--- 2022/rust/day15-beacon-exclusion-zone/src/main.rs
+++ 2022/rust/day15-beacon-exclusion-zone/src/main.rs
@@ -1,11 +1,112 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::VecDeque;
 use std::io::{self, Read};
 
 fn main() {
     let input = read_stdin();
     let sensors = parse(input);
-    solve1(sensors, 2000000);
+    //solve1(sensors, 2000000);
+    solve2(&sensors, 20);
+}
+
+fn solve2(sensors: &Vec<Sensor>, max: i32) {
+    let (min_x, min_y) = (0, 0);
+    let (max_x, max_y) = (max, max);
+
+    let mut ranges: Vec<Vec<Range>> = vec![];
+
+    for y in min_y..=max_y {
+        let mut y_range: Vec<Range> = vec![];
+
+        for s in sensors.iter() {
+            let dy = i32::abs(s.coords.y - y);
+            if dy > s.radius {
+                continue;
+            }
+            let dx = (s.radius - dy) / 2;
+
+            y_range.push(Range {
+                left: std::cmp::max(s.coords.x - dx, 0),
+                right: std::cmp::min(s.coords.x + dx, max_x),
+            })
+        }
+
+        dbg!(&y_range);
+        let merged = Range::merge(&y_range);
+        dbg!(merged);
+        break;
+    }
+
+    dbg!(ranges);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
+struct Range {
+    left: i32,
+    right: i32,
+}
+
+impl Range {
+    fn merge(ranges: &Vec<Range>) -> Vec<Range> {
+        let mut ranges = ranges.clone();
+        ranges.sort();
+        ranges.reverse(); // walk it from the back, because we can't pop front
+
+        let mut merged: Vec<Range> = Vec::new();
+
+        while let Some(r) = ranges.pop() {
+            if merged.is_empty() {
+                merged.push(r);
+                continue;
+            }
+
+            // if doesn't overlap, new interval
+            if merged[0].right < r.left {
+                merged.push(r);
+            } else if merged[0].right < r.right {
+                let merged_range = Range {
+                    left: merged[0].left,
+                    right: r.right,
+                };
+                merged.pop();
+                merged.push(merged_range);
+            }
+        }
+        merged
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Sensor {
+    coords: Point,
+    beacon: Point,
+    radius: i32,
+}
+
+impl Sensor {
+    fn in_range(&self, p: &Point) -> bool {
+        self.coords.manhattan_distance(p) <= self.radius
+    }
+}
+
+fn cave_max_min_x(sensors: &Vec<Sensor>) -> (i32, i32) {
+    let max_x = sensors.iter().map(|s| s.coords.x + s.radius).max().unwrap();
+    let min_x = sensors.iter().map(|s| s.coords.x - s.radius).min().unwrap();
+
+    (min_x, max_x)
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn manhattan_distance(&self, p: &Point) -> i32 {
+        i32::abs(self.x - p.x) + i32::abs(self.y - p.y)
+    }
 }
 
 fn solve1(sensors: Vec<Sensor>, row: i32) {
@@ -61,38 +162,6 @@ fn parse(input: String) -> Vec<Sensor> {
     }
 
     sensors
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Sensor {
-    coords: Point,
-    beacon: Point,
-    radius: i32,
-}
-
-impl Sensor {
-    fn in_range(&self, p: &Point) -> bool {
-        self.coords.manhattan_distance(p) <= self.radius
-    }
-}
-
-fn cave_max_min_x(sensors: &Vec<Sensor>) -> (i32, i32) {
-    let max_x = sensors.iter().map(|s| s.coords.x + s.radius).max().unwrap();
-    let min_x = sensors.iter().map(|s| s.coords.x - s.radius).min().unwrap();
-
-    (min_x, max_x)
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl Point {
-    fn manhattan_distance(&self, p: &Point) -> i32 {
-        i32::abs(self.x - p.x) + i32::abs(self.y - p.y)
-    }
 }
 
 // Thou Shalt Not Compile Regular Expressions In A Loop
