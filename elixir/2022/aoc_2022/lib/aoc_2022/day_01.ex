4ba2500e0e5d5b4b3dd8a2f2aa5b3a0701b862ba
kellan <kellan@pobox.com>
2023-11-22 18:40:57 -0500

    part 2

--- elixir/2022/aoc_2022/lib/aoc_2022/day_01.ex
+++ elixir/2022/aoc_2022/lib/aoc_2022/day_01.ex
@@ -16,15 +16,36 @@ defmodule Aoc2022.Day01 do
   end
 
   def part2() do
-    IO.puts("Hello")
+    Aoc2022.process_input(@day)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&sum_lines/1)
+    |> Enum.sort()
+    |> Enum.take(-3)
+    |> Enum.sum()
   end
 end
 
 defmodule Mix.Tasks.Day01 do
   use Mix.Task
 
-  def run(_) do
+  def run([]) do
     solution = Aoc2022.Day01.part1()
     IO.puts(solution)
+  end
+
+  def run([arg]) do
+    case arg do
+      "part1" ->
+        solution = Aoc2022.Day01.part1()
+        IO.puts(solution)
+
+      "part2" ->
+        solution = Aoc2022.Day01.part2()
+        IO.puts(solution)
+
+      _ ->
+        IO.puts("Unknown argument: #{arg}")
+        System.halt(1)
+    end
   end
 end
