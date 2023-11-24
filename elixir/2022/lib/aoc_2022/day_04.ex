7cb89477b566c107bbc317292dae3935e74d3037
kellan <kellan@pobox.com>
2023-11-24 15:24:28 -0500

    day 4, 2022

---
+++ elixir/2022/lib/aoc_2022/day_04.ex
@@ -0,0 +1,46 @@
+defmodule Aoc2022.Day04 do
+  @day "day04"
+
+  def part1() do
+    Aoc2022.process_input(@day, :split)
+    |> Enum.map(&contains?/1)
+    |> Enum.sum()
+  end
+
+  def contains?(pair) do
+    [left, right] = String.split(pair, ",")
+    [l1, l2] = String.split(left, "-") |> Enum.map(&String.to_integer/1)
+    [r1, r2] = String.split(right, "-") |> Enum.map(&String.to_integer/1)
+
+    if (l1 <= r1 && l2 >= r2) or (r1 <= l1 && r2 >= l2) do
+      1
+    else
+      0
+    end
+  end
+end
+
+defmodule Mix.Tasks.Day04 do
+  use Mix.Task
+
+  def run([]) do
+    run(["part1"])
+  end
+
+  def run([arg]) do
+    case arg do
+      "part1" ->
+        solution = Aoc2022.Day04.part1()
+
+        IO.puts(solution)
+
+      "part2" ->
+        solution = Aoc2022.Day04.part2()
+        IO.puts(solution)
+
+      _ ->
+        IO.puts("Unknown argument: #{arg}")
+        System.halt(1)
+    end
+  end
+end
