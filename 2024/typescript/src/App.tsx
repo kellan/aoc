1485afa92b39fcbb0c0a8fb08c18c0c1c8159291
kellan <kellan@pobox.com>
2024-12-07 16:57:13 -0500

    run day 01

--- 2024/typescript/src/App.tsx
+++ 2024/typescript/src/App.tsx
@@ -3,32 +3,77 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const solutions = import.meta.glob('/days/*/solution.ts')
+const testInputs = import.meta.glob('/days/*/test.txt', { as: 'raw' })
+const realInputs = import.meta.glob('/days/*/input.txt', { as: 'raw' })
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [results, setResults] = useState<{
+    day: string,
+    part1Test: number,
+    part2Test: number,
+    part1: number,
+    part2: number
+  }>({
+    day: '',
+    part1Test: 0,
+    part2Test: 0,
+    part1: 0,
+    part2: 0
+  })
+
+  const runSolution = async (day: string) => {
+    if (solutions[`/days/${day}/solution.ts`]) {
+      const module = await solutions[`/days/${day}/solution.ts`]()
+      const testData = await testInputs[`/days/${day}/test.txt`]()
+      const realData = await realInputs[`/days/${day}/input.txt`]()
+
+      setResults({
+        day: day,
+        part1Test: module.part1(testData),
+        part2Test: module.part2(testData),
+        part1: module.part1(realData),
+        part2: module.part2(realData)
+      })
+    }
+  }
+
+  runSolution('01')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app" >
+      <nav className="sidebar">
+        <ul>
+          <li><a href="#home">Foo</a></li>
+          <li><a href="#dashboard">Dashboard</a></li>
+          <li><a href="#settings">Settings</a></li>
+        </ul>
+      </nav>
+
+      <main className="main-content">
+        <header>
+          <h1>Day {results.day}</h1>
+        </header>
+        <div className="content">
+          <table border="1" cellPadding="10">
+            <thead>
+              <tr>
+                <th></th><th>Part 1</th><th>Part 2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test</td><td>{results.part1Test}</td><td>{results.part2Test}</td>
+              </tr>
+              <tr>
+                <td>Real</td><td></td><td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main >
+    </div >
   )
 }
 
