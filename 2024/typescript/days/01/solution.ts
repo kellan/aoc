be5de65f077d181435038077988588350276fbdd
kellan <kellan@pobox.com>
2024-12-07 16:56:44 -0500

    store the solutions, input and test input here

---
+++ 2024/typescript/days/01/solution.ts
@@ -0,0 +1,9 @@
+
+export function part1(input: string): number {
+  const lines = input.split("\n")
+  return 1;
+}
+export function part2(input: string): number {
+  const lines = input.split("\n")
+  return 21;
+}
