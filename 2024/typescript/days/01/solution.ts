9cec395d5f440a1fd11318171d237a44707afdbc
kellan <kellan@pobox.com>
2024-12-07 17:24:20 -0500

    day 1, part 1

--- 2024/typescript/days/01/solution.ts
+++ 2024/typescript/days/01/solution.ts
@@ -1,8 +1,21 @@
 
 export function part1(input: string): number {
-  const lines = input.split("\n")
-  return lines.length;
+
+  const rows = input.trim().split("\n").map((row) => {
+    return row.trim().split(/\s+/).map(Number)
+  })
+
+  const [left, right] = [
+    rows.map((row) => row[0]).sort(),
+    rows.map((row) => row[1]).sort()
+  ]
+
+  return left.reduce(
+    (acc, curr, i) => acc + Math.abs(curr - right[i]),
+    0
+  )
 }
+
 export function part2(input: string): number {
   const lines = input.split("\n")
   return lines.length * 2;
