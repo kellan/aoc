018e41895f789f9e09dd6c61090767cdbd4fc444
kellan <kellan@pobox.com>
2024-12-13 18:22:37 -0500

    day 5, part 2

--- 2024/typescript/days/05/solution.ts
+++ 2024/typescript/days/05/solution.ts
@@ -24,6 +24,25 @@ function isCorrect(rule: string[], update: string[]): boolean {
 }
 
 export function part2(input: string): number {
+  const [rulesInput, updatesInput] = input.split('\n\n')
+  const rules = rulesInput.split('\n').map(el => { return el.split('|') })
 
-  return 2 * 2
+  const updates = updatesInput.split('\n').map(el => { return el.split(',') })
+
+  const incorrectUpdates = updates.filter(update => { return !rules.every(rule => isCorrect(rule, update)) })
+  //console.log("incorrectUpdates", incorrectUpdates)
+
+  const sortedUpdates = incorrectUpdates.map(update => {
+    return update.toSorted((a, b) => {
+      return rules.some(rule => {
+        return rule[0] === a && rule[1] === b
+      }) ? -1 : 1
+    })
+  })
+
+  let results = sortedUpdates.reduce((acc, curr) => acc + parseInt(curr[Math.floor(curr.length / 2)]), 0)
+
+  //console.log("sortedUpdates", sortedUpdates)
+
+  return results
 }
