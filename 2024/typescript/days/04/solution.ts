f99ebac7f69e73259d99157328aa5df2109f4314
kellan <kellan@pobox.com>
2024-12-13 16:42:03 -0500

    part 2

--- 2024/typescript/days/04/solution.ts
+++ 2024/typescript/days/04/solution.ts
@@ -18,7 +18,7 @@ export function part1(input: string): number {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       if (grid[i][j] === xmas[0]) {
-        let delta = checkXmas(grid, i, j)
+        let delta = checkXmas(xmas, grid, i, j)
         found = found + delta
       }
     }
@@ -27,19 +27,20 @@ export function part1(input: string): number {
   return found
 }
 
-function checkXmas(grid: string[][], i: number, j: number): number {
+function checkXmas(word: string, grid: string[][], i: number, j: number): number {
   let found = 0
 
   for (const [dx, dy] of dir) {
     let x = i
     let y = j
-    let k = 1
+    let k = 0
     while (k < xmas.length) {
-      x += dx
-      y += dy
+
       if (x < 0 || x >= grid.length || y < 0 || y >= grid[x].length || grid[x][y] !== xmas[k]) {
         break
       }
+      x += dx
+      y += dy
       k++
     }
     if (k === xmas.length) {
@@ -50,7 +51,38 @@ function checkXmas(grid: string[][], i: number, j: number): number {
   return found
 }
 
-export function part2(input: string): number {
+const dir2 = [
+  [-1, -1],
+  [-1, 1],
+  [1, -1],
+  [1, 1],
+]
 
-  return 2 * 4
+const word = 'MAS'
+
+export function part2(input: string): number {
+  const grid = input.trim().split('\n').map((row) => row.split(''))
+
+  let found = 0
+
+  for (let i = 1; i < grid.length - 1; i++) {
+    for (let j = 1; j < grid[i].length - 1; j++) {
+
+      if (grid[i][j] === 'A') {
+
+        const diagonals = [
+          grid[i - 1][j - 1],
+          grid[i - 1][j + 1],
+          grid[i + 1][j - 1],
+          grid[i + 1][j + 1],
+        ].join('')
+
+        if (['MSMS', 'MMSS', 'SSMM', 'SMSM'].includes(diagonals)) {
+          found++
+        }
+      }
+    }
+  }
+
+  return found
 }
