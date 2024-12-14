9562184d30a73351c1940ad4bab6e41b259c3baf
kellan <kellan@pobox.com>
2024-12-14 16:19:02 -0500

    day 7, part 2

--- 2024/typescript/days/07/solution.ts
+++ 2024/typescript/days/07/solution.ts
@@ -5,7 +5,7 @@ export function part1(input: string): number {
 
     for (let calibration of calibrations) {
         let [target, ...nums] = calibration
-        let possible_values = permutate(nums)
+        let possible_values = permutate(nums, ['*', '+'])
         if (possible_values.includes(target)) {
             results += target
         }
@@ -14,14 +14,22 @@ export function part1(input: string): number {
     return results
 }
 
-function permutate(nums: number[]): number[] {
+function permutate(nums: number[], operators: string[]): number[] {
     let accumulations: number[] = [nums.shift()]
+    //console.log(accumulations, nums)
 
     for (let i = 0; i < nums.length; i++) {
         let new_acc = []
         for (let j = 0; j < accumulations.length; j++) {
-            new_acc.push(accumulations[j] * nums[i])
-            new_acc.push(accumulations[j] + nums[i])
+            for (let op of operators) {
+                if (op === '*') {
+                    new_acc.push(accumulations[j] * nums[i])
+                } else if (op === '+') {
+                    new_acc.push(accumulations[j] + nums[i])
+                } else if (op === 'concat') {
+                    new_acc.push(parseInt("" + accumulations[j] + nums[i]))
+                }
+            }
         }
         accumulations = new_acc
     }
@@ -30,5 +38,19 @@ function permutate(nums: number[]): number[] {
 
 
 export function part2(input: string): number {
-    return 2
+    const calibrations = input.trim().split('\n').map((row) => row.split(/: | /).map(Number))
+    //let calibrations = [[156, 15, 6]]
+
+    let results = 0
+
+    for (let calibration of calibrations) {
+        let [target, ...nums] = calibration
+        let possible_values = permutate(nums, ['*', '+', 'concat'])
+
+        if (possible_values.includes(target)) {
+            results += target
+        }
+    }
+
+    return results
 }
