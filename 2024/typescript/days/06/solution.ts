4fc988064f0830e5e8fab2b91690888a5fd2801a
kellan <kellan@pobox.com>
2024-12-14 12:26:09 -0500

    part 1, refactored

--- 2024/typescript/days/06/solution.ts
+++ 2024/typescript/days/06/solution.ts
@@ -8,13 +8,18 @@ const dir = [
 
 export function part1(input: string): number {
     const grid = input.trim().split('\n').map((row) => row.split(''))
-    let positions = 0
     let guardRow = grid.findIndex((row) => row.includes('^'));
     let guardCol = grid[guardRow].indexOf('^');
     let visited = new Set([`${guardRow},${guardCol}`])
     let currentDir = 0
 
-    while (WebTransportDatagramDuplexStream) {
+    run(grid, guardRow, guardCol, visited, currentDir)
+
+    return visited.size
+}
+
+function run(grid: string[][], guardRow: number, guardCol: number, visited: Set<string>, currentDir: number): Set<string> {
+    while (true) {
         let dx = dir[currentDir][0]
         let dy = dir[currentDir][1]
         let nextRow = guardRow + dx
@@ -35,12 +40,19 @@ export function part1(input: string): number {
         visited.add(`${guardRow},${guardCol}`)
     }
 
-    return visited.size
+    return visited
 }
 
 export function part2(input: string): number {
     const grid = input.trim().split('\n').map((row) => row.split(''))
     let positions = 0
+
+    let guardRow = grid.findIndex((row) => row.includes('^'));
+    let guardCol = grid[guardRow].indexOf('^');
+    let visited = new Set([`${guardRow},${guardCol}`])
+    let currentDir = 0
+
+    run(grid, guardRow, guardCol, visited, currentDir)
 
     return positions
 }
