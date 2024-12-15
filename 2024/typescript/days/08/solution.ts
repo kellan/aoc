bd934f5acfa07cb2ec52f8beb8bc8a7805f181d3
kellan <kellan@pobox.com>
2024-12-14 19:40:06 -0500

    day 8, part 1

---
+++ 2024/typescript/days/08/solution.ts
@@ -0,0 +1,61 @@
+
+type Antenna = {
+    x: number,
+    y: number,
+    frequency: String
+}
+
+export function part1(input: string): number {
+    const grid = input.trim().split('\n').map((row) => row.split(''))
+    const height = grid.length
+    const width = grid[0].length
+
+    let antenna: Map<String, Antenna[]> = new Map()
+
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            if (grid[y][x] !== '.') {
+                let freq = grid[y][x]
+                if (!antenna.has(freq)) {
+                    antenna.set(freq, [])
+                }
+                antenna.get(freq)?.push({ x: x, y: y, frequency: freq })
+            }
+        }
+    }
+
+    let antinodes = new Map<String, [number, number]>()
+
+    for (let [freq, positions] of antenna) {
+        let pairs = generatePairs(positions)
+        pairs.forEach((pair) => {
+            let anodes = generateAntinodes(pair, width, height)
+            anodes.forEach((an) => {
+                let key = `${an[0]},${an[1]}`
+                antinodes.set(key, an)
+            })
+        })
+    }
+    //console.log(antinodes)
+    return antinodes.size
+}
+
+function generateAntinodes(pair: [Antenna, Antenna], width: number, height: number): [number, number][] {
+    let antinodes: [number, number][] = []
+    let [dx, dy] = [pair[1].x - pair[0].x, pair[1].y - pair[0].y]
+    antinodes = [
+        [pair[0].x + 2 * dx, pair[0].y + 2 * dy],
+        [pair[1].x - 2 * dx, pair[1].y - 2 * dy]
+    ]
+    antinodes = antinodes.filter(([x, y]) => x >= 0 && y >= 0 && x < width && y < height)
+    return antinodes
+}
+
+function generatePairs(arr: Antenna[]): [Antenna, Antenna][] {
+    return arr.flatMap((x, i) => arr.slice(i + 1).map((y) => [x, y]));
+}
+
+export function part2(input: string): number {
+    return 2
+}
