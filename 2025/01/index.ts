6d5ad5aa329a45efbf7eddafcf7ed3fe50d1033d
kellan <kelliott@adobe.com>
2025-12-01 19:34:25 -0500

    inline tests, still not working for part 2

--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -7,16 +7,17 @@ import { countGroups } from "../util/count";
 import { test, expect } from "bun:test";
 
 
-function turnDial(startPos: Number, instruction: string) {
-  const [dir, ...digits] = instruction
-  const rotation = (dir === 'R') ? Number(digits.join('')) : Number(digits.join(''))*-1
-  let newPos = startPos+rotation
+export function turnDial(startPos: Number, instruction: string) {
+  const [dir, digits] = [instruction[0], Number(instruction.slice(1))]
+  const rotation = (dir === 'R') ? digits : digits*-1
 
-  let clicks = Math.abs(Math.floor(newPos/100))
+  let newPos = startPos+rotation
+  let clicks = Math.floor(Math.abs(newPos)/100)
   newPos = newPos % 100
   if (newPos < 0) {
     newPos = 100 + newPos
   }
+  
   return [newPos, clicks]
 }
 
@@ -39,32 +40,39 @@ export function part2(input: string) {
   let clicks = 0
   for (const l of lines(input)) {
     [pos, click] = turnDial(pos, l)
-    if (click) {
-      clicks += 1
-    }
+    clicks += click
+    console.log(l, pos, click, clicks)
   }
   return clicks
 }
 
 
-runCli({
-    dir: import.meta.dir,
-    part1,
-    part2
+if (import.meta.main && process.env.NODE_ENV !== "test") {
+  runCli({
+      dir: import.meta.dir,
+      part1,
+      part2
+  });
+}
+
+test("day01 part1 sample", () => {
+  expect(mapToInt(["1", "2", "3", "4 "])).toEqual([1, 2, 3, 4]);
 });
 
-// test("day01 part1 sample", () => {
-//   expect(mapToInt(["1", "2", "3", "4 "])).toEqual([1, 2, 3, 4]);
-// });
+test("day01 turnDial 11 R8", () => {
+  expect(turnDial(11, "R8")).toEqual([19,0])
+});
 
-// test("day01 turnDial 11 R8", () => {
-//   expect(turnDial(11, "R8")).toBe(19)
-// });
+test("day01 turnDial 0 L1", () => {
+  expect(turnDial(0, "L1")).toEqual([99,0])
+});
 
-// test("day01 turnDial 0 L1", () => {
-//   expect(turnDial(0, "L1")).toBe(99)
-// });
+test("day01 turnDial 99 R1", () => {
+  expect(turnDial(99, "R1")).toEqual([0,1])
+});
 
-// test("day01 turnDial 99 R1", () => {
-//   expect(turnDial(99, "R1")).toBe(0)
-// });
+test("day01 turnDial 50 R1000", () => {
+  expect(turnDial(50, "R1000")).toEqual([50,10])
+});
+
+// 1 1 2 2 3 4 4 5 6
