3a4661d925e4991ad7c56e2a8d2565b1b37abf63
kellan <kelliott@adobe.com>
2025-12-21 11:32:17 -0500

    part 2 working, needed to deal with ranges wholey within a larger range (math.max)

--- 2025/05/index.ts
+++ 2025/05/index.ts
@@ -10,7 +10,7 @@ export function part1(input: string) {
 
     for (const i of ingredients) {
         for (const r of ranges) {
-            if (i >= r[0] && i <= r[1]) {
+            if (contains(i, r)) {
                 fresh.push(i)
                 break
             }
@@ -20,7 +20,44 @@ export function part1(input: string) {
     return fresh.length
 }
 
+function contains(i: number, r: number[]): boolean {
+    return i >= r[0] && i <= r[1]
+}
+
 export function part2(input: string) {
+    const inputs = input.split("\n\n")
+    const ranges = lines(inputs[0]).map(s => s.split("-").map(Number))
+
+    ranges.sort((a, b) => a[0] - b[0]);
+
+    //console.log(ranges)
+
+    const S = 0
+    const E = 1
+
+    let consolidated = []
+
+    for (let i=0;i<ranges.length;i++) {
+        if (i == ranges.length-1) {
+            consolidated.push(ranges[i])
+        } else if (ranges[i][E] < ranges[i+1][S]) {
+            consolidated.push(ranges[i])
+        } else {
+            ranges[i+1][S] = ranges[i][S]
+            ranges[i+1][E] = Math.max(ranges[i][E], ranges[i+1][E])
+        }
+    }
+
+    //console.log(consolidated)
+    //for (const )
+
+    let total = 0
+    for (const r of consolidated) {
+       // console.log(r[0], r[1])
+        total += r[1] - r[0] + 1
+    }
+
+    return total
 }
 
 if (import.meta.main && process.env.NODE_ENV !== "test") {
